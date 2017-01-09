42195b1a6c115a1214f7d42895c6938fbcd99fe0
אבירן <@DESKTOP-BKBHGVG>
2017-01-09 23:06:27 +0200

    add ui and service and app.js

---
+++ MyFood/Scripts/Angular/app.js
@@ -0,0 +1,10 @@
+﻿var app = angular.module('myFoodApp', ['ngRoute', 'ngResource']);
+app.config(function($routeProvider){
+
+    $routeProvider.when('/Add', {
+        templateUrl: 'Partials/AddView.html'
+    })
+    .otherwise({
+        templateUrl: 'Partials/MainView.html'
+    })
+});
