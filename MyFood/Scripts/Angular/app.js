40885d01f7e068c0b8c9f316a0371e8511e608e9
אבירן <@DESKTOP-BKBHGVG>
2017-01-19 22:14:17 +0200

    add model login, add recipe page, fixing the rout of pages, design master page

--- MyFood/Scripts/Angular/app.js
+++ MyFood/Scripts/Angular/app.js
@@ -4,6 +4,9 @@ app.config(function ($routeProvider) {
     $routeProvider.when('/Add', {
         templateUrl: 'Partials/AddView.html'
     })
+    $routeProvider.when('/Recipe', {
+        templateUrl: 'Partials/RecipeView.html'
+    })
     .otherwise({
         templateUrl: 'Partials/MainView.html'
     })
