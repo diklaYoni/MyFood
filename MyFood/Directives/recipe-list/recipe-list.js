9ce43f7fa144d64b3aa408d4d0d472cdcbea0a8a
diklooly <diklooly@gmail.com>
2017-01-21 22:43:32 +0200

    הוספת התצוגה להוספת מתכון+ פקד להצגת רשימת מתכונים

---
+++ MyFood/Directives/recipe-list/recipe-list.js
@@ -0,0 +1,33 @@
+﻿app.directive('recipeList', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'Directives/recipe-list/recipe-list.html',
+        scope: {
+            data: '='
+        },
+        link: function (scope) {
+            scope.listItems = [];
+            scope.HoveredItem = -1;
+
+            scope.addItem = function () {
+
+                if (scope.newItem && scope.newItem.replace(" ", "") != "") {
+                    scope.listItems.push(scope.newItem);
+                    scope.newItem = "";
+                }
+            }
+
+            scope.removeItem = function (p_item) {
+                scope.listItems.splice(scope.listItems.indexOf(p_item), 1);
+            }
+
+            scope.mouseEnterItem = function (p_index) {
+                scope.HoveredItem = p_index;
+            }
+
+            scope.mouseLeaveItem = function () {
+                scope.HoveredItem = -1;
+            }
+        }
+    };
+});
