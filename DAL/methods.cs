3d8e899e7a77c4aadb5825ded19fa29049fe9bd1
nati2fridman <nati2fridman@gmail.com>
2017-02-14 22:05:07 +0200

    add function to add user

--- DAL/methods.cs
+++ DAL/methods.cs
@@ -23,5 +23,34 @@ namespace DAL
             Recipe recRecipe = mfeEntity.Recipe.Find(nId);
             return recRecipe;
         }
+
+        /// <summary>
+        /// Add new user to the system
+        /// </summary>
+        /// <param name="strEmail"> user email </param>
+        /// <param name="strPassword"> user password </param>
+        /// <param name="strName"> user name </param>
+        /// <param name="strGender"> user gender </param>
+        /// <returns> return if the user added or not </returns>
+        public bool AddUser(string strEmail, string strPassword, string strName, string strGender)
+        {
+            bool bIsAdded = false;
+
+            if ((strEmail != String.Empty) && (strPassword != String.Empty) && (strName != String.Empty) && (strGender != String.Empty))
+            {
+                User usrUser = new User();
+                usrUser.EMAIL = strEmail;
+                usrUser.Password = strPassword;
+                usrUser.Name = strName;
+
+                ////////  why gender is bool???
+                //usrUser.Gender = strGender;
+
+                mfeEntity.User.Add(usrUser);
+                bIsAdded = true;
+            }
+
+            return bIsAdded;
+        }
     }
 }
